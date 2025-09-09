40bc8eb415a060b87dc06df8427d3daaa079e4f0
EstesOwen <owene@gmail.com>
2025-09-08 19:43:56 -0500

    keeping track of mult in main

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import time
-time.time()
-current_time = 0
-game_speed = 1
-if current_time == 10:
-    game_speed *= 1.1
+# get the starting time and initialize the current time
+starting_time = time.time()
+current_time = time.time()
+# create a variable to keep track of our jumping multiplier
+mult = 0
+
+
+# main game loop
+while(True):
+    #get the new time
+    current_time = time.time()
+    if(current_time - starting_time) % 10:
+        mult += 0.1
+    
